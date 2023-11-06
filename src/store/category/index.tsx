aa012770cb307289b7d9ec8b3ca8bbcf2d14c577
Nhật Quang <donhatquang010102@gmail.com>
2023-11-06 14:25:28 +0700

    .

--- src/store/category/index.tsx
+++ src/store/category/index.tsx
@@ -28,7 +28,7 @@ export const CategoryFacade = () => {
     putDisable: (values: { id: string; disable: boolean }) => dispatch(action.putDisable(values)),
     delete: (id: string) => dispatch(action.delete(id)),
     getCategory: () => dispatch(action.getCategory()),
-    getByIdCategory: () => dispatch(action.getByIdCategory(id:String)),
+    getByIdCategory:  ({ fullTextSearch, id }: { fullTextSearch: string; id: string })=> dispatch(action.getByIdCategory(id)),
   };
 };
 export class Category extends CommonEntity {
